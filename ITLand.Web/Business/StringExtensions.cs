1a3d048abdb1a73f0a020d9dcfdef6d46ada6c94
Sinan Altaii <sinan.altaii@creuna.se>
2019-01-20 20:01:19 +0100

    adds stingextensions

---
+++ ITLand.Web/Business/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace ITLand.Web.Business
+{
+    public static class StringExtensions
+    {
+        public static string TruncateAtWord(this string input, int noWords)
+        {
+            var output = string.Empty;
+            var inputArr = input.Split(' ');
+
+            if (inputArr.Length <= noWords)
+                return input;
+
+            if (noWords > 0)
+            {
+                for (var i = 0; i < noWords; i++)
+                {
+                    output += inputArr[i] + " ";
+                }
+
+                output += "...";
+                return output;
+            }
+
+            return input;
+        }
+    }
+}
