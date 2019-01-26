05d82b541122cf77ae88f92b6c9672d863f71d50
Sinan Altaii <sinan.altaii@creuna.se>
2019-01-26 09:20:30 +0100

    added start page template, and sitegroupnames

---
+++ ITLand.Web/Models/Pages/StartPageType.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace ITLand.Web.Models.Pages
+{
+	[ContentType(
+		DisplayName = "Start page",
+		GUID = "1deb2cf0-b5a5-4a4c-a19d-f8ea8eeaba96",
+		GroupName = SiteGroupNames.Specialized,
+		Description = "The home page for a website with an area for blocks and partial pages.")]
+	public class StartPageType : PageData
+	{
+		[CultureSpecific]
+		[Display(Name = "Start page heading", Description = "Heading",
+			GroupName = SystemTabNames.Content,
+			Order = 10)]
+		public virtual string Heading { get; set; }
+
+		[CultureSpecific]
+		[Display(
+			Name = "Main body",
+			Description = "The main body containing content, text and etc.",
+			GroupName = SystemTabNames.Content,
+			Order = 20)]
+		public virtual XhtmlString MainBody { get; set; }
+	}
+}
