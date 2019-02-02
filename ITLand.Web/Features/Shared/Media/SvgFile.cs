efdc34f8321ef07b4030207290c886b570d5ea2d
Sinan Altaii <sinan.altaii@creuna.se>
2019-02-02 21:59:12 +0100

    enables media  files uploads

---
+++ ITLand.Web/Features/Shared/Media/SvgFile.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.Blobs;
+using EPiServer.Framework.DataAnnotations;
+
+namespace ITLand.Web.Features.Shared.Media
+{
+	[ContentType(
+		DisplayName = "Svg File", 
+		GUID = "bf1f53ab-2534-4f74-bb1f-928a95816bcf", 
+		Description = "Use to upload svg file")]
+	[MediaDescriptor(ExtensionString = "svg")]
+	public class SvgFile : MediaData
+	{
+		public override Blob Thumbnail => base.BinaryData;
+	}
+}
