2e07c1bdfc6f05ae5d2ab8ed72bcdd7c49585937
Sinan Altaii <sinan.altaii@limetta.se>
2019-12-07 11:04:23 +0100

    fixed http namespace not found

--- ITLand.Web/Global.asax.cs
+++ ITLand.Web/Global.asax.cs
@@ -1,24 +1,37 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace ITLand.Web
 {
-	public class Global : EPiServer.Global
-	{
-		protected void Application_Start()
-		{
-			var razorViewEngine = ViewEngines.Engines.OfType<RazorViewEngine>().First();
-			RazorViewEngineConfig.RegisterViewLocations(razorViewEngine);
-			RazorViewEngineConfig.RegisterPartialViewLocations(razorViewEngine);
-			AreaRegistration.RegisterAllAreas();
-			MvcHandler.DisableMvcResponseHeader = true;
-		}
+    public class Global : EPiServer.Global
+    {
+        protected void Application_Start()
+        {
+            var razorViewEngine = ViewEngines.Engines.OfType<RazorViewEngine>().First();
+            RazorViewEngineConfig.RegisterViewLocations(razorViewEngine);
+            RazorViewEngineConfig.RegisterPartialViewLocations(razorViewEngine);
+            AreaRegistration.RegisterAllAreas();
+            MvcHandler.DisableMvcResponseHeader = true;
 
-		protected override void RegisterRoutes(RouteCollection routes)
-		{
-			base.RegisterRoutes(routes);
-			RouteConfig.RegisterRoutes(routes);
-		}
-	}
+            //var user = Membership.CreateUser("admin", "!QAZxsw2", "sinan.altaii@live.se");
+            //user.IsApproved = true;
+            //if (!Roles.RoleExists("Administrators"))
+            //{
+            //    Roles.CreateRole("Administrators");
+            //}
+
+            //if (!Roles.IsUserInRole("admin", "Administrators"))
+            //{
+            //    Roles.AddUserToRole("admin", "Administrators");
+            //}
+        }
+
+        protected override void RegisterRoutes(RouteCollection routes)
+        {
+            base.RegisterRoutes(routes);
+            RouteConfig.RegisterRoutes(routes);
+        }
+    }
 }
